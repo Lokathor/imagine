17c329c19a6be3817ee1166ca2368d135f3bcabf
Lokathor <zefria@gmail.com>
2021-11-30 15:03:11 -0700

    added demo window displayer.

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,73 +1,72 @@
-use fermium::{error::*, events::*, video::*, *};
-use gl33::*;
 
-fn get_err_msg_and_panic() {
-  unsafe {
-    let mut v = Vec::with_capacity(4096);
-    let mut p = SDL_GetErrorMsg(v.as_mut_ptr(), v.capacity() as _);
-    while *p != 0 {
-      print!("{}", *p as u8 as char);
-      p = p.add(1);
+use pixels::{Error, Pixels, SurfaceTexture};
+use winit::{
+  dpi::LogicalSize,
+  event::{Event, WindowEvent},
+  event_loop::{ControlFlow, EventLoop},
+  window::WindowBuilder,
+};
+
+use imagine::{png::*, RGBA8};
+
+fn main() -> Result<(), Error> {
+  const GLIDER_BIG_RAINBOW: &[u8] = include_bytes!("glider-big-rainbow.png");
+
+  let (rgba8, width, height) = match parse_me_a_png_yo(GLIDER_BIG_RAINBOW) {
+    Ok((rgba8, width, height)) => (rgba8, width, height),
+    Err(e) => panic!("Error: {:?}", e),
+  };
+  //
+  let event_loop = EventLoop::new();
+  let window = {
+    let size = LogicalSize::new(width as f64, height as f64);
+    WindowBuilder::new()
+      .with_title("Demo window")
+      .with_inner_size(size)
+      .with_min_inner_size(size)
+      .build(&event_loop)
+      .unwrap()
+  };
+
+  let mut pixels = {
+    let window_size = window.inner_size();
+    let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+    Pixels::new(width, height, surface_texture)?
+  };
+
+  event_loop.run(move |event, _, control_flow| match event {
+    Event::RedrawRequested(_) => {
+      pixels.get_frame().copy_from_slice(bytemuck::cast_slice(&rgba8));
+      if pixels.render().map_err(|e| println!("pixels.render() failed: {}", e)).is_err() {
+        *control_flow = ControlFlow::Exit;
+      }
+      window.request_redraw();
     }
-    println!();
-    panic!();
-  }
+    Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
+      *control_flow = ControlFlow::Exit;
+    }
+    _ => (),
+  });
 }
 
-fn main() {
-  // TODO: read a PNG filename to display from the command line.
+fn parse_me_a_png_yo(png: &[u8]) -> Result<(Vec<RGBA8>, u32, u32), PngError> {
+  let mut it = RawPngChunkIter::new(png).map(PngChunk::try_from).filter(critical_errors_only);
+  let ihdr =
+    it.next().ok_or(PngError::NoChunksPresent)??.to_ihdr().ok_or(PngError::FirstChunkNotIHDR)?;
 
-  unsafe {
-    assert_eq!(0, SDL_Init(SDL_INIT_VIDEO));
-    assert_eq!(0, SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3));
-    assert_eq!(0, SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3));
-    assert_eq!(
-      0,
-      SDL_GL_SetAttribute(
-        SDL_GL_CONTEXT_PROFILE_MASK,
-        SDL_GL_CONTEXT_PROFILE_CORE.0 as _
-      )
-    );
-
-    // TODO: the window size should be based on the PNG file we're reading.
-    // TODO: window title should be based on the PNG file we're reading.
-
-    let win = SDL_CreateWindow(
-      b"imagine fermium demo\0".as_ptr().cast(),
-      50,
-      50,
-      800,
-      600,
-      (SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL).0 as _,
-    );
-    if win.is_null() {
-      get_err_msg_and_panic();
-    }
-    let ctx = SDL_GL_CreateContext(win);
-    if ctx.0.is_null() {
-      get_err_msg_and_panic();
-    }
-    let gl =
-      GlFns::load_from(&|c_char_ptr| SDL_GL_GetProcAddress(c_char_ptr.cast())).unwrap();
-    gl.ClearColor(0.2, 0.3, 0.3, 1.0);
-
-    // TODO: setup for displaying an image
-
-    let mut event: SDL_Event = core::mem::zeroed();
-    'program: loop {
-      while SDL_PollEvent(&mut event) != 0 {
-        if event.common.type_ == SDL_QUIT as _ {
-          break 'program;
-        }
-      }
-
-      gl.Clear(GL_COLOR_BUFFER_BIT);
-
-      // TODO: draw the image
-
-      SDL_GL_SwapWindow(win);
-    }
-    SDL_DestroyWindow(win);
-    SDL_Quit();
-  }
+  let idat_slice_it = idat_peek.filter_map(|r_chunk| match r_chunk {
+    Ok(PngChunk::IDAT(IDAT { data })) => Some(data),
+    _ => None,
+  });
+  decompress_idat_to_temp_storage(&mut temp_memory_buffer, idat_slice_it)?;
+  //
+  let mut vec = Vec::new();
+  vec.resize((ihdr.width * ihdr.height) as usize, RGBA8::default());
+  //
+  unfilter_decompressed_data(ihdr, &mut &mut temp_memory_buffer, |x, y, data| {
+    //println!("x: {x}, y: {y}, data: {data:?}", x = x, y = y, data = data);
+    vec[(y * ihdr.width + x) as usize] = bytemuck::cast_slice(data)[0];
+  })?;
+  //
+  Ok((vec, ihdr.width, ihdr.height))
 }
