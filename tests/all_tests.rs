ecf9dee1b7101451a0981e19d134a9cee8c02224
Lokathor <zefria@gmail.com>
2022-08-08 11:01:19 -0600

    make tests able to potentially support no-default-features (but no actual tests do yet)

--- tests/all_tests.rs
+++ tests/all_tests.rs
@@ -1,7 +1,9 @@
 #![allow(bad_style)]
 
+#[cfg(feature = "png")]
 mod png;
 
+#[allow(dead_code)]
 fn rand_bytes(count: usize) -> Vec<u8> {
   let mut buffer = vec![0; count];
   getrandom::getrandom(&mut buffer).unwrap();
