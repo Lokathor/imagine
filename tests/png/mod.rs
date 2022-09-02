f921e0e77dbdd134231c7b81ed2e04b3435c7634
lokathor <zefria@gmail.com>
2022-09-01 23:43:36 -0600

    expand testing somewhat.

--- tests/png/mod.rs
+++ tests/png/mod.rs
@@ -1,10 +1,25 @@
-use imagine::png::PngRawChunkIter;
+use imagine::{pixels::RGBA8888, png::PngRawChunkIter, Image};
 use walkdir::WalkDir;
 
 #[test]
 fn test_RawPngChunkIter_no_panics() {
+  // even totally random data should never panic the iterator!
+  for _ in 0..10 {
+    let v = super::rand_bytes(1024);
+    for _ in PngRawChunkIter::new(&v) {
+      //
+    }
+  }
+}
+
+#[test]
+#[cfg(all(feature = "alloc", feature = "miniz_oxide"))]
+fn test_decode_test_pngs() {
   // iter ALL files in the test folder, even non-png files shouldn't panic it.
   for entry in WalkDir::new("tests/").into_iter().filter_map(|e| e.ok()) {
+    if entry.file_type().is_dir() {
+      continue;
+    }
     println!("{}", entry.path().display());
     let v = match std::fs::read(entry.path()) {
       Ok(v) => v,
@@ -13,15 +28,8 @@ fn test_RawPngChunkIter_no_panics() {
         continue;
       }
     };
-    for _ in PngRawChunkIter::new(&v) {
-      //
-    }
-  }
-  // even totally random data should never panic the iterator!
-  for _ in 0..10 {
-    let v = super::rand_bytes(1024);
-    for _ in PngRawChunkIter::new(&v) {
-      //
-    }
+    let _image_result = Image::<RGBA8888>::try_from_png_bytes(&v);
+    // Most test images are "hostile" so they naturally fail to parse.
+    // However, the library shouldn't panic even with a hostile image.
   }
 }
