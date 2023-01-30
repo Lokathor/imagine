a65a25bb0ebe4b75a3aa60a83c4ca8726a646b03
Lokathor <zefria@gmail.com>
2023-01-30 10:18:35 -0700

    parses paletted data now (i think).

--- src/bmp.rs
+++ src/bmp.rs
@@ -122,6 +122,7 @@ pub enum BmpError {
   AllocError,
   PixelDataIllegalLength,
   PixelDataIllegalRLEContent,
+  NotAPalettedBmp,
   /// The BMP file might be valid, but either way this library doesn't currently
   /// know how to parse it.
   ParserIncomplete,
@@ -130,7 +131,7 @@ pub enum BmpError {
 use bytemuck::cast;
 
 use crate::{
-  image::Bitmap,
+  image::{Bitmap, Palmap},
   pixel_formats::{sRGBIntent, RGBA8888},
 };
 
@@ -1723,7 +1724,7 @@ where
   ///
   /// ## Failure
   /// Errors include, but are not limited to:
-  /// * Incorrect Bmp File Header
+  /// * Incorrect Bmp File Header.
   /// * Allocation failure.
   #[cfg_attr(docs_rs, doc(cfg(all(feature = "bmp", feature = "miniz_oxide"))))]
   pub fn try_from_bmp_bytes(bytes: &[u8]) -> Result<Self, BmpError> {
@@ -2237,8 +2238,361 @@ impl<P> crate::image::Palmap<u8, P>
 where
   P: From<RGBA8888> + Clone,
 {
+  /// Attempts to make a [Palmap](crate::image::Palmap) from BMP bytes.
+  ///
+  /// ## Failure
+  /// Errors include, but are not limited to:
+  /// * Incorrect Bmp File Header.
+  /// * Allocation failure.
   #[cfg_attr(docs_rs, doc(cfg(all(feature = "bmp", feature = "miniz_oxide"))))]
-  pub fn try_from_bmp_bytes(bytes: &[u8]) -> Option<Self> {
-    todo!()
+  pub fn try_from_bmp_bytes(bytes: &[u8]) -> Result<Self, BmpError> {
+    use alloc::vec::Vec;
+    use bytemuck::cast_slice;
+    use core::mem::size_of;
+    //
+    let (file_header, rest) = BmpFileHeader::try_from_bytes(bytes)?;
+    if file_header.total_file_size as usize != bytes.len()
+      || !(COMMON_BMP_TAGS.contains(&file_header.tag))
+    {
+      return Err(BmpError::ThisIsProbablyNotABmpFile);
+    }
+    let (info_header, mut rest) = BmpInfoHeader::try_from_bytes(rest)?;
+    let compression = info_header.compression();
+    let bits_per_pixel = info_header.bits_per_pixel() as usize;
+    let width: u32 = info_header.width().unsigned_abs();
+    let height: u32 = info_header.height().unsigned_abs();
+    let pixel_count: usize = width.saturating_mul(height) as usize;
+
+    #[allow(unused_assignments)]
+    let palette: Vec<P> = match info_header.palette_len() {
+      0 => Vec::new(),
+      pal_count => {
+        let mut v: Vec<RGBA8888> = Vec::new();
+        v.try_reserve(pal_count).map_err(|_| BmpError::AllocError)?;
+        match info_header {
+          BmpInfoHeader::Core(_) => {
+            let bytes_needed = pal_count * size_of::<[u8; 3]>();
+            let (pal_slice, new_rest) = if rest.len() < bytes_needed {
+              return Err(BmpError::InsufficientBytes);
+            } else {
+              rest.split_at(bytes_needed)
+            };
+            rest = new_rest;
+            let pal_slice: &[[u8; 3]] = cast_slice(pal_slice);
+            for [b, g, r] in pal_slice.iter().copied() {
+              v.push(RGBA8888 { r, g, b, a: 0xFF });
+            }
+          }
+          _ => {
+            let bytes_needed = pal_count * size_of::<[u8; 4]>();
+            let (pal_slice, new_rest) = if rest.len() < bytes_needed {
+              return Err(BmpError::InsufficientBytes);
+            } else {
+              rest.split_at(bytes_needed)
+            };
+            rest = new_rest;
+            let pal_slice: &[[u8; 4]] = cast_slice(pal_slice);
+            for [b, g, r, a] in pal_slice.iter().copied() {
+              v.push(RGBA8888 { r, g, b, a });
+            }
+            if v.iter().copied().all(|c| c.a == 0) {
+              v.iter_mut().for_each(|c| c.a = 0xFF);
+            }
+          }
+        }
+        // unfortunately we need to double-allocate if we want to keep the generics.
+        let mut pal_final: Vec<P> = Vec::new();
+        v.try_reserve(pal_count).map_err(|_| BmpError::AllocError)?;
+        pal_final.extend(v.iter().map(|rgba| P::from(*rgba)));
+        pal_final
+      }
+    };
+
+    let indexes: Vec<u8> = {
+      let mut v = Vec::new();
+      v.try_reserve(pixel_count).map_err(|_| BmpError::AllocError)?;
+      v.resize(pixel_count, 0_u8);
+      v
+    };
+
+    let mut palmap: Palmap<u8, P> = Palmap { width, height, indexes, palette };
+
+    let pixel_data_start_index: usize = file_header.pixel_data_offset as usize;
+    let pixel_data_end_index: usize = pixel_data_start_index + info_header.pixel_data_len();
+    let pixel_data = if bytes.len() < pixel_data_end_index {
+      return Err(BmpError::InsufficientBytes);
+    } else {
+      &bytes[pixel_data_start_index..pixel_data_end_index]
+    };
+
+    match compression {
+      BmpCompression::RgbNoCompression
+      | BmpCompression::Bitfields
+      | BmpCompression::AlphaBitfields => {
+        let bits_per_line: usize =
+          bits_per_pixel.saturating_mul(info_header.width().unsigned_abs() as usize);
+        let no_padding_bytes_per_line: usize =
+          (bits_per_line / 8) + (((bits_per_line % 8) != 0) as usize);
+        let bytes_per_line: usize =
+          ((no_padding_bytes_per_line / 4) + ((no_padding_bytes_per_line % 4) != 0) as usize) * 4;
+        debug_assert!(no_padding_bytes_per_line <= bytes_per_line);
+        debug_assert_eq!(bytes_per_line % 4, 0);
+        if (pixel_data.len() % bytes_per_line) != 0
+          || (pixel_data.len() / bytes_per_line) != (info_header.height().unsigned_abs() as usize)
+        {
+          return Err(BmpError::PixelDataIllegalLength);
+        }
+        //
+
+        match bits_per_pixel {
+          1 | 2 | 4 => {
+            let (base_mask, base_down_shift) = match bits_per_pixel {
+              1 => (0b1000_0000, 7),
+              2 => (0b1100_0000, 6),
+              4 => (0b1111_0000, 4),
+              _ => unreachable!(),
+            };
+            let mut per_row_op = |i: &mut dyn Iterator<Item = (usize, &[u8])>| {
+              for (y, data_row) in i {
+                let y = y as u32;
+                let mut x = 0;
+                for byte in data_row.iter().copied() {
+                  let mut mask: u8 = base_mask;
+                  let mut down_shift: usize = base_down_shift;
+                  while mask != 0 && x < width {
+                    let pal_index = (byte & mask) >> down_shift;
+                    if let Some(i) = palmap.get_mut(x, y) {
+                      *i = pal_index;
+                    }
+                    //
+                    mask >>= bits_per_pixel;
+                    down_shift = down_shift.wrapping_sub(bits_per_pixel);
+                    x += 1;
+                  }
+                }
+              }
+            };
+            if info_header.height() < 0 {
+              per_row_op(&mut pixel_data.chunks_exact(bytes_per_line).enumerate());
+            } else {
+              per_row_op(&mut pixel_data.rchunks_exact(bytes_per_line).enumerate());
+            }
+          }
+          8 => {
+            let mut per_row_op = |i: &mut dyn Iterator<Item = (usize, &[u8])>| {
+              for (y, data_row) in i {
+                for (x, pal_index) in
+                  data_row[..no_padding_bytes_per_line].iter().copied().enumerate()
+                {
+                  if let Some(i) = palmap.get_mut(x as u32, y as u32) {
+                    *i = pal_index;
+                  }
+                }
+              }
+            };
+            if info_header.height() < 0 {
+              per_row_op(&mut pixel_data.chunks_exact(bytes_per_line).enumerate());
+            } else {
+              per_row_op(&mut pixel_data.rchunks_exact(bytes_per_line).enumerate());
+            }
+          }
+          24 => {
+            return Err(BmpError::NotAPalettedBmp);
+          }
+          16 => {
+            return Err(BmpError::NotAPalettedBmp);
+          }
+          32 => {
+            return Err(BmpError::NotAPalettedBmp);
+          }
+          _ => return Err(BmpError::IllegalBitDepth),
+        }
+      }
+      BmpCompression::RgbRLE8 => {
+        // For the RLE encodings, there's either "encoded" pairs of bytes, or
+        // "absolute" runs of bytes that are also always even in length (with a
+        // padding byte if necessary). Thus, no matter what, the number of bytes
+        // in the pixel data should always be even when we're processing RLE data.
+        if pixel_data.len() % 2 != 0 {
+          return Err(BmpError::PixelDataIllegalLength);
+        }
+        let mut it = cast_slice::<u8, [u8; 2]>(pixel_data).iter().copied();
+        // Now the MSDN docs get kinda terrible. They talk about "encoded" and
+        // "absolute" mode, but whoever wrote that is bad at writing docs. What
+        // we're doing is we'll pull off two bytes at a time from the pixel data.
+        // Then we look at the first byte in a pair and see if it's zero or not.
+        //
+        // * If the first byte is **non-zero** it's the number of times that the second
+        //   byte appears in the output. The second byte is an index into the palette,
+        //   and you just put out that color and output it into the bitmap however many
+        //   times.
+        // * If the first byte is **zero**, it signals an "escape sequence" sort of
+        //   situation. The second byte will give us the details:
+        //   * 0: end of line
+        //   * 1: end of bitmap
+        //   * 2: "Delta", the *next* two bytes after this are unsigned offsets to the
+        //     right and up of where the output should move to (remember that this mode
+        //     always describes the BMP with a bottom-left origin).
+        //   * 3+: "Absolute", The second byte gives a count of how many bytes follow
+        //     that we'll output without repetition. The absolute output sequences
+        //     always have a padding byte on the ending if the sequence count is odd, so
+        //     we can keep pulling `[u8;2]` at a time from our data and it all works.
+        let mut x: u32 = 0;
+        let mut y: u32 = height - 1;
+        'iter_pull_rle8: while let Some([count, pal_index]) = it.next() {
+          if count > 0 {
+            // data run of the `pal_index` value.
+            for _ in 0..count {
+              if let Some(i) = palmap.get_mut(x, y) {
+                *i = pal_index;
+              }
+              x += 1;
+            }
+          } else {
+            match pal_index {
+              0 => {
+                // end of line.
+                x = 0;
+                y = y.saturating_sub(1);
+              }
+              1 => {
+                // end of bitmap
+                break 'iter_pull_rle8;
+              }
+              2 => {
+                // position delta
+                if let Some([d_right, d_up]) = it.next() {
+                  x += u32::from(d_right);
+                  y = y.saturating_sub(u32::from(d_up));
+                } else {
+                  return Err(BmpError::PixelDataIllegalRLEContent);
+                }
+              }
+              mut raw_count => {
+                while raw_count > 0 {
+                  // process two bytes at a time, which we'll call `q` and `w` for
+                  // lack of better names.
+                  if let Some([q, w]) = it.next() {
+                    // q byte
+                    if let Some(i) = palmap.get_mut(x, y) {
+                      *i = q;
+                    }
+                    x += 1;
+
+                    // If `raw_count` is only 1 then we don't output the `w` byte.
+                    if raw_count >= 2 {
+                      // w byte
+                      if let Some(i) = palmap.get_mut(x, y) {
+                        *i = w;
+                      }
+                      x += 1;
+                    }
+                  } else {
+                    return Err(BmpError::PixelDataIllegalRLEContent);
+                  }
+                  //
+                  raw_count = raw_count.saturating_sub(2);
+                }
+              }
+            }
+          }
+        }
+      }
+      BmpCompression::RgbRLE4 => {
+        // RLE4 works *basically* how RLE8 does, except that every time we
+        // process a byte as a color to output then it's actually two outputs
+        // instead (upper bits then lower bits). The stuff about the escape
+        // sequences and all that is still the same sort of thing.
+        if pixel_data.len() % 2 != 0 {
+          return Err(BmpError::PixelDataIllegalLength);
+        }
+        let mut it = cast_slice::<u8, [u8; 2]>(pixel_data).iter().copied();
+        //
+        let mut x = 0;
+        let mut y = height - 1;
+        //
+        'iter_pull_rle4: while let Some([count, pal_index]) = it.next() {
+          if count > 0 {
+            // in this case, `count` is the number of indexes to output, and
+            // `pal_index` is *two* pixel indexes (high bits then low bits). We'll
+            // write the pair of them over and over in a loop however many times.
+            let h: u8 = pal_index >> 4;
+            let l: u8 = pal_index & 0b1111;
+            let count = count;
+            for _ in 0..count {
+              if let Some(i) = palmap.get_mut(x, y) {
+                *i = h;
+              }
+              if let Some(i) = palmap.get_mut(x + 1, y) {
+                *i = l;
+              }
+              x += 2;
+            }
+          } else {
+            // If the count is zero then we use the same escape sequence scheme as
+            // with the RLE8 format.
+            match pal_index {
+              0 => {
+                // end of line.
+                //print!("RLE4: == END OF LINE: before (x: {}, y: {})", x, y);
+                x = 0;
+                y = y.saturating_sub(1);
+                //println!(" after (x: {}, y: {})", x, y);
+              }
+              1 => {
+                // end of bitmap
+                //println!("RLE4: ==>> END OF THE BITMAP");
+                break 'iter_pull_rle4;
+              }
+              2 => {
+                // delta
+                if let Some([d_right, d_up]) = it.next() {
+                  x += u32::from(d_right);
+                  y = y.saturating_sub(u32::from(d_up));
+                } else {
+                  return Err(BmpError::PixelDataIllegalRLEContent);
+                }
+              }
+              mut raw_count => {
+                // in this case, we'll still have raw outputs for a sequence, but
+                // `raw_count` is the number of indexes, and each 2 bytes in the
+                // sequence is **four** output indexes. The only complication is
+                // that we need to be sure we stop *as soon* as `raw_count` hits 0
+                // because otherwise we'll mess up our `x` position (ruining all
+                // future outputs on this scanline, and possibly ruining other
+                // scanlines or something).
+                while raw_count > 0 {
+                  if let Some([q, w]) = it.next() {
+                    for pal_index in [(q >> 4), (q & 0b1111), (w >> 4), (w & 0b1111)] {
+                      if let Some(i) = palmap.get_mut(x, y) {
+                        *i = pal_index;
+                      }
+                      x += 1;
+                      raw_count = raw_count.saturating_sub(1);
+                      if raw_count == 0 {
+                        break;
+                      }
+                    }
+                  } else {
+                    return Err(BmpError::PixelDataIllegalRLEContent);
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      // Note(Lokathor): Uh, I guess "the entire file is inside the 'pixel_array'
+      // data" or whatever? We need example files that use this compression before
+      // we can begin to check out what's going on here.
+      BmpCompression::Jpeg => return Err(BmpError::ParserIncomplete),
+      BmpCompression::Png => return Err(BmpError::ParserIncomplete),
+      // Note(Lokathor): probably we never need to support this until someone asks?
+      BmpCompression::CmykNoCompression => return Err(BmpError::ParserIncomplete),
+      BmpCompression::CmykRLE4 => return Err(BmpError::ParserIncomplete),
+      BmpCompression::CmykRLE8 => return Err(BmpError::ParserIncomplete),
+    }
+
+    Ok(palmap)
   }
 }
